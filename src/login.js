7214c8489353fe9da28f93d955525dfaf4083041
Wesley Xiao <wex057@ucsd.edu>
2019-05-12 22:53:52 -0700

    basic front end for login

---
+++ src/login.js
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Bootstrap from "react-bootstrap";
+
+import "./Login.css"
+
+export default class Login extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+	    	email: "",
+	    	password: ""
+	    };
+	}
+
+  	validateForm() {
+    	return this.state.email.length > 0 && this.state.password.length > 0;
+ 	}
+
+  	handleChange = event => {
+    	this.setState({
+      		[event.target.id]: event.target.value
+    	});
+  	}
+
+	handleSubmit = event => {
+		event.preventDefault();
+	}
+
+	render() {
+		return (
+		<Form>
+		  <Form.Group controlId="formBasicEmail">
+		    <Form.Label>Email address</Form.Label>
+		    <Form.Control type="email" placeholder="Enter email" />
+		    
+		  </Form.Group>
+
+		  <Form.Group controlId="formBasicPassword">
+		    <Form.Label>Password</Form.Label>
+		    <Form.Control type="password" placeholder="Password" />
+		  </Form.Group>
+		  <Form.Group controlId="formBasicChecbox">
+		    <Form.Check type="checkbox" label="Check me out" />
+		  </Form.Group>
+		  <Button variant="primary" type="submit">
+		    Submit
+		  </Button>
+		</Form>
+		);
+	}
+	/*
+  	render() {
+		return (
+      		<div className="Login">
+        		<Form onSubmit={this.handleSubmit}>
+          			<Form.Group controlId="email" bsSize="large">
+          				<Form.label>Email</Form.label>
+            			<Form.Control
+              				autoFocus
+              				type="email"
+              				placeholder="enter email"
+              				value={this.state.email}
+              				onChange={this.handleChange}
+            			/>
+          			</Form.Group>
+          			<Form.Group controlId="password" bsSize="large">
+          				<Form.label>Password</Form.label>
+            			<Form.Control
+              				value={this.state.password}
+              				onChange={this.handleChange}
+              				type="password"
+              				placeholder="password"
+            			/>
+          			</Form.Group>
+			        <Button
+			          block
+			          bsSize="large"
+			          disabled={!this.validateForm()}
+			          type="submit"
+			        >
+            			Login
+          			</Button>
+        		</Form>
+      		</div>
+    	);
+  	}*/
+}
