23285b2cd8517ad9208c5ffaeab33fa8b9ff9e90
trevor <t1perez@ucsd.edu>
2019-05-19 23:48:37 -0700

    Added routing to the application

---
+++ src/components/Profile/profile.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import fire from '../Fire/fire';
+import Nav from '../Navbar/navbar';
+import '../../styles/user.css';
+
+export default class Profile extends Component {
+
+  constructor(props){
+    super(props);
+
+  }
+
+  render() {
+    return (
+
+        <div id="profileContainer">
+            <div id="userNameArea">
+            <h1 className="userName">User Name</h1>
+            </div>
+                <div id="profilePicArea">
+                <img className="profilePic" src={ require('../../images/corgi.png') } height="300px" width="300px" />
+            </div>
+            <ul className="profileBar">
+                <li><a id="prof" href="#settings">Settings</a></li>
+                <li><a id="prof" href="#contact">Contact Support</a></li>
+                <li><a id="prof" href="#logout" style={{color:'red'}}>Logout</a></li>
+            </ul>
+        </div>
+    )
+}
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../src/components/App/app.js';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import {
+  Route,
+  NavLink,
+  BrowserRouter as Router,
+  Switch
+} from "react-router-dom";
+import App from "./components/App/app";
+import Profile from "./components/Profile/profile";
+import Login from "./components/Login/login";
+import Signup from "./components/Signup/signup";
 import * as serviceWorker from './serviceWorker';
+import Notfound from "./components/NotFound/NotFound";
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const routing = (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <NavLink exact activeClassName="active" to="/">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="active" to="/profile">
+            Profile
+          </NavLink>
+        </li>
+        <li>
+            <NavLink activeClassName="active" to="/login">
+                Login
+            </NavLink>
+        </li>
+        <li>
+            <NavLink activeClassName="active" to="/signup">
+                Sign up
+            </NavLink>
+        </li>
+      </ul>
+      <hr />
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <Route component={Notfound} />
+      </Switch>
+    </div>
+  </Router>
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(routing, document.getElementById("root"));
+
+
 serviceWorker.unregister();
