962d5792056f1e0bb51194a93f8f1c40ac1c845a
Nick <nickaguilarnew@gmail.com>
2019-05-20 13:13:55 -0700

    get from firestore kind of working

--- src/components/Pantry/pantry.js
+++ src/components/Pantry/pantry.js
@@ -6,27 +6,59 @@ export default class Pantry extends Component{
     constructor(props){
         super(props);
         this.db = fire.firestore();
-        //hardcoded to my account for testing, will have to get user email later
-        var favorites = this.db.collection('users').doc('nraguila.test@ucsd.edu');
-        var items = favorites.get().then(doc => {
-            if(!doc.exists){
-                console.log("no doc");
-            }
-            else{
-                let arr = doc.getString("test");
-                ReactDOM.render(arr, document.getElementById("main"));
-            }
-        }).catch(err => {
-            console.log("err", err);
+        this.ref = fire.firestore().collection('users');
+        this.unsubscribe = null;
+        this.state = {
+            users: []
+        };
+
+        // this.ref.on('value', dataSnapshot => {
+        //     let items = [];
+        //     dataSnapshot.forEach(childSnapshot => {
+        //         let item = childSnapshot.val();
+        //         item['.key'] = childSnapshot.key;
+        //         items.push(item);
+        //     })
+        //     this.setState({items});
+        // })
+    }
+    onCollectionUpdate = (querySnapshot) => {
+        const users = [];
+
+        querySnapshot.forEach((doc) => {
+            const{ favorites } = doc.data();
+            users.push({
+                key: doc.id,
+                doc,
+                favorites
+            });
+        });
+        this.setState({
+            users
         });
     }
 
+    componentDidMount(){
+        this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate);
+    }
     render(){
+        // const records = this.state.items.map(items => 
+        //     <tr key={items.favorites}>
+        //         <td>{items.favorites}</td>
+        //     </tr>
+        //     );
         return(
             <div id="main">
                 Pantry
+                <body>
+                    {this.state.users.map(user => 
+                        <tr>
+                            <td>{user.favorites}</td>
+                        </tr>
+                        )}
+                </body>
             </div>
-        )
+        );
     }
 
 
