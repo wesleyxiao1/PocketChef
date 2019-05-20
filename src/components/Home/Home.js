be0e7d792ee8ee5303535804610a50d6415370d2
Patrick Tan <patrick4499@gmail.com>
2019-05-19 20:02:44 -0700

    converted html to jsx

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import fire from '../Fire/fire';
 import Nav from '../Navbar/navbar';
+import '../../styles/index.css'
 
 export default class Home extends Component {
 
@@ -17,7 +18,25 @@ export default class Home extends Component {
   render() {
     return (
       <div className="Home">
-        <h1>You are Home</h1>
+
+        <title>PocketChef</title>
+          {/* navigation bar */}
+          <div>
+            <ul className="navigationbar">
+              <li><a href="#home">Home</a></li>
+              <li><a href="#pantry">Pantry</a></li>
+              <li><a href="#contact">Profile</a></li>
+              {/* login button */}
+              <li style={{float: 'right'}}><button onclick="document.getElementById('id01').style.display='block'" id="login">Login</button></li>
+            </ul>
+
+
+        <div className="search-box">
+          <input type="text" name className="search-txt" placeholder="Search Recipes" />
+          <a className="search-btn" href="#"><i className="fas fa-search" /></a>
+        </div>
+      </div>
+
         {/*TODO Put these buttons in a navbar, make it look nice*/}
         {/* <ul>
           <li><button id="pantry" onClick={this.goToPantry}>Pantry</button></li>
