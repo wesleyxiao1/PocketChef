1a0662a84d586bb9ed991da76cb9a2e0d01bbe32
Nick <nickaguilarnew@gmail.com>
2019-05-20 17:13:17 -0700

    generalized email field

--- src/components/Pantry/pantry.js
+++ src/components/Pantry/pantry.js
@@ -6,57 +6,38 @@ export default class Pantry extends Component{
     constructor(props){
         super(props);
         this.db = fire.firestore();
-        this.ref = fire.firestore().collection('users');
-        this.docRef = fire.firestore().collection('users').doc('nraguila@ucsd.edu');
-        this.unsubscribe = null;
+        //need to get user email and pass into the reference below
+        this.email = fire.auth().currentUser.email;
+        this.docRef = fire.firestore().collection('users').doc(this.email);
         this.docUnsub = null;
+
         this.state = {
-            users: [],
-            favorites: []
+            favorites: [],
+            pantry: []
         };
     }
 
     onDocumentUpdate = (documentSnapshot) => {
-        let favorites = documentSnapshot.get('favorites');
+        //TODO: Handle empty pantry
+        let favorites = documentSnapshot.get('pantry');
         this.setState({favorites});
     }
 
-    onCollectionUpdate = (querySnapshot) => {
-        const users = [];
-
-        querySnapshot.forEach((doc) => {
-            const{ favorites } = doc.data();
-            users.push({
-                key: doc.id,
-                doc,
-                favorites
-            });
-        });
-        this.setState({
-            users
-        });
-    }
-
     componentDidMount(){
-        this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate);
         this.docUnsub = this.docRef.onSnapshot(this.onDocumentUpdate);
     }
     render(){
-        // const records = this.state.items.map(items => 
-        //     <tr key={items.favorites}>
-        //         <td>{items.favorites}</td>
-        //     </tr>
-        //     );
+
         return(
             <div id="main">
                 Pantry
-                <body>
+                <h1>
                     {this.state.favorites.map(fav => 
                         <tr>
                             <td>{fav}</td>
                         </tr>
                         )}
-                </body>
+                </h1>
             </div>
         );
     }
