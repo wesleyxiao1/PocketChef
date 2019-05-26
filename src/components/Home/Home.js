0809968aa1cd3122205647050a7c5c76c483f900
sam <melero>
2019-05-26 12:14:57 -0700

    search semi works. need to style.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,18 +7,16 @@ import Footer from '../Footer/footer'
 import Nav from '../Navbar/navbar';
 import '../../styles/home.css';
 import axios from 'axios';
-
 import Pantry from '../Pantry/pantry';
+
 export default class Home extends Component {
 
   constructor(props){
     super(props);
     this.logoutFirebase = this.logoutFirebase.bind(this);
     this.state = {
-      //username: ''
-      recipeName: ''
+      data: []
     };
-
     this.handleClick = this.handleClick.bind(this);
   }
   goToPantry(current){
@@ -29,21 +27,30 @@ export default class Home extends Component {
       fire.auth().signOut();
   }
 
-  /*handleClick () {
-    axios.get('https://api.edamam.com/search?q=chicken&app_id=4863ac07&app_key=6e58a756abe12ad9122ba4525c78f6b9&from=0&to=3&calories=59')
-      .then(response => this.setState({recipeName: response.hits.recipe.label}))
+  /*getRecipes = async () =>{
+    return await axios({url: 'https://api.edamam.com/search?q=chicken&app_id=4863ac07&app_key=6e58a756abe12ad9122ba4525c78f6b9&from=0&to=3&calories=59'})
   }*/
 
-  handleClick () {
-    axios.get('https://www.food2fork.com/api/search?key=65ab939ee06267a743713a544290c2a2&q=shredded%20chicken')
-      .then(response => this.setState({recipeName: response.recipes[1].title}))
+  /*handleClick() {
+    (async () => {
+      const recipes = await getRecipes()
+      console.log(recipes.data)
+    })()}
+  */
+
+  handleClick() {
+    fetch(`https://www.food2fork.com/api/search?key=65ab939ee06267a743713a544290c2a2&q=shredded%20chicken`)
+      .then(res => res.json())
+      .then(json => this.setState({ data: json.recipes }));
   }
 
-  /*
-  handleClick () {
-    axios.get('https://api.github.com/users/maecapozzi')
-      .then(response => this.setState({username: response.data.name}))
-    } */  
+    /*axios.get('https://api.edamam.com/search?q=chicken&app_id=4863ac07&app_key=6e58a756abe12ad9122ba4525c78f6b9&from=0&to=3&calories=59')
+      //.then((data) => this.setState({results: data.hits[0].recipe.label}))
+      */
+  /*handleClick () {
+    axios.get('https://www.food2fork.com/api/search?key=65ab939ee06267a743713a544290c2a2&q=shredded%20chicken')
+      .then(response => this.setState({recipeName: response.data.recipes[0].title}))
+  }*/
 
   render() {
     return (
@@ -59,31 +66,30 @@ export default class Home extends Component {
               <li style={{float: 'right'}}><button type="LogOut" onClick={this.logoutFirebase} class="LoginButton" id="login">LogOut</button></li>
             </ul>
           </div>
-        {/*<div className="search-box">
-          <input type="text" name className="search-txt" placeholder="Search Recipes" />
-          <a className="search-btn" href="#"><i className="fas fa-search" /></a>
+          <div className='button__container'>
+            <button className='button' onClick={this.handleClick}>
+              Search
+            </button>
+          </div>
+          <div className="searchResults">
+            <ul className="results">
+              {/*this.state.data.recipes.map(el => (*/}              
+              {this.state.data.map(function(recipe, i){
+                  return <li key={i}>
+                    <div>
+                      <a>{recipe.title}</a>
+                    </div>
+                    <div style="float: left;">
+                        <img src={recipe.image_url}></img>
+                      </div>
+                  </li>
+              })
+              }
+            </ul>
+          </div>
         </div>
-        */}
-        <div className='button__container'>
-          <button className='button' onClick={this.handleClick}>
-            Search
-          </button>
-          <p>{this.state.recipeName}</p>
-        </div>
-
-        {/*TODO Put these buttons in a navbar, make it look nice*/}
-        {/* <ul>
-          <li><button id="pantry" onClick={this.goToPantry}>Pantry</button></li>
-          <li><button id="list" onclick={this.goToList}>List</button></li>
-          <li><button id="recipes" onClick={this.goToRecipes}>Recipes</button></li>
-          <li><button id="favorites" onClick={this.goToFavorites}>Favorites</button></li>
-          <li><button id="profile" onClick={this.goToProfile}>Profile</button></li>
-        </ul> 
-        <Nav/>*/}
-      }
-        
-        </div>
-      );
-    
+      ); 
+    } 
   }
-}
+
+  //ReactDOM.render(<Home />, document.getElementById("home"));
