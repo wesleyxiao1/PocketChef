016d07cee8720f2a57d27729573eb4afd2246e2a
kresler <kresler5012@gmail.com>
2019-05-30 15:18:25 -0700

    visuals

--- src/components/Pantry/pantry.js
+++ src/components/Pantry/pantry.js
@@ -16,11 +16,14 @@ const PantryPage = () => (
 class PantryItemsBase extends Component {
   constructor(props) {
     super(props);
-    this.addToDataBase = this.addToDataBase.bind(this);
+    // console.log(this.props);
+    // const authUser= this.props.firebase.auth;
+    // const uid = this.authUser.O;
     this.state = {
       text: '',
       loading: false,
       localpantry: [],
+      authUid: this.props.firebase.auth.O
     };
   }
 
@@ -42,22 +45,21 @@ class PantryItemsBase extends Component {
     this.props.firebase.pantry_item(uid).remove();
   };
 
-  onCreatePantryItem = (event, authUser) => {
+  onCreatePantryItem = (event) => {
+
     this.state.localpantry.push(this.state.text);
-    this.state.text='';
+    this.state.text = '';
     this.setState({
       text: ''
     });
-    console.log(this.state.localpantry);
-    this.addToDataBase(authUser);
-    event.preventDefault();
-  };
-
-  addToDataBase(authUser){
-    this.props.firebase.pantry_items(authUser.uid).update({
+    // console.log(this.state.localpantry);
+    // console.log(this.props.firebase);
+    this.props.firebase.user(this.state.authUid).update({
       pantry_items: this.state.localpantry
     });
-  }
+
+    event.preventDefault();
+  };
 
   componentDidMount() {
     this.setState({ loading: true });
@@ -89,134 +91,164 @@ class PantryItemsBase extends Component {
   }
 
   render() {
-    const { text, pantry_items, loading } = this.state;
-
+    // const { text, loading, localpantry } = this.state;
+    // const { authUser, pantry_item, onRemovePantryItem } = this.props;
+    // const authUser= this.props.firebase.auth
     return (
-      <AuthUserContext.Consumer>
-        {authUser => (
-          <div>
-            {loading && <div>Loading ...</div>}
+      // <AuthUserContext.Consumer>
+      //   {authUser => (
+      //     <div>
+      //       {loading && <div>Loading ...</div>}
 
-            {pantry_items ? (
-              <PantryList
-                authUser={authUser}
-                pantry_items={pantry_items}
-                onRemovePantryItem={this.onRemovePantryItem}
-                onEditPantryItem={this.onEditPantryItem}
-              />
-            ) : (
-                <div>There are no pantry items ...</div>
-              )}
-            <form onSubmit={event => this.onCreatePantryItem(event, authUser)}>
-              <input
-                type="text"
-                value={text}
-                onChange={this.onChangeText}
-              />
-              <button type="submit">Send</button>
-            </form>
+      //       {localpantry ? (
+      //         <PantryList
+      //           authUser={authUser}
+      //           pantry_items={localpantry}
+      //           onRemovePantryItem={this.onRemovePantryItem}
+      //           onEditPantryItem={this.onEditPantryItem}
+      //         />
+      //       ) : (
+      //           <div>There are no pantry items ...</div>
+      //         )}
+      //       <form onSubmit={event => this.onCreatePantryItem(event, authUser)}>
+      //         <input
+      //           type="text"
+      //           value={text}
+      //           onChange={this.onChangeText}
+      //         />
+      //         <button type="submit">Send</button>
+      //       </form>
 
-          </div>
-        )}
-      </AuthUserContext.Consumer>
+      //     </div>
+      //   )}
+      // </AuthUserContext.Consumer>
+      // <AuthUserContext.Consumer>
+      <div id="main">
+        {/* Pantry */}
+        <h2>
+          {this.state.localpantry.map(ingredient =>
+            <tr>
+              <td>{ingredient}</td>
+            </tr>
+          )}
+        </h2>
+        <input
+          type="text"
+          placeholder="Enter ingredient to add"
+          name="newIngredient"
+          id="ingredient-input"
+          value={this.state.text}
+          onChange={this.onChangeText}
+          required />
+        <button
+          type="submit"
+          id="additem"
+          onClick={this.onCreatePantryItem}
+          class="btn btn-primary"
+        >
+          Add to Pantry
+                  </button>
+      </div>
+      // </AuthUserContext.Consumer> 
+
     );
   }
 }
 
-const PantryList = ({
-  authUser,
-  pantry_items,
-  onEditPantryItem,
-  onRemovePantryItem
-}) => (
-    <ul>
-      {pantry_items.map(pantry_item => (
-        <PantryItem
-          authUser={authUser}
-          key={pantry_item.uid}
-          pantry_item={pantry_item}
-          onRemovePantryItem={onRemovePantryItem}
-          onEditPantryItem={onEditPantryItem}
-        />
-      ))}
-    </ul>
-  );
+// const PantryList = ({
+//   authUser,
+//   pantry_items,
+//   onEditPantryItem,
+//   onRemovePantryItem
+// }) => (
+//     <ul>
+//       {pantry_items.map(pantry_item => (
+//         <PantryItem
+//           authUser={authUser}
+//           key={pantry_item.uid}
+//           pantry_item={pantry_item}
+//           onRemovePantryItem={onRemovePantryItem}
+//           onEditPantryItem={onEditPantryItem}
+//         />
+//       ))}
+//     </ul>
+//   );
 
-class PantryItem extends Component {
-  constructor(props) {
-    super(props);
+// class PantryItem extends Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      editMode: false,
-      editText: this.props.pantry_item.text,
-    };
-  }
+//     this.state = {
+//       editMode: false,
+//       editText: this.props.pantry_item.text,
+//     };
+//   }
 
-  onToggleEditMode = () => {
-    this.setState(state => ({
-      editMode: !state.editMode,
-      editText: this.props.pantry_item.text,
-    }));
-  };
+//   onToggleEditMode = () => {
+//     this.setState(state => ({
+//       editMode: !state.editMode,
+//       editText: this.props.pantry_item.text,
+//     }));
+//   };
 
-  onSaveEditText = () => {
-    this.props.onEditPantryItem(this.props.pantry_item, this.state.editText);
+//   onSaveEditText = () => {
+//     this.props.onEditPantryItem(this.props.pantry_item, this.state.editText);
 
-    this.setState({ editMode: false });
-  };
+//     this.setState({ editMode: false });
+//   };
 
-  onChangeEditText = event => {
-    this.setState({ editText: event.target.value });
-  };
+//   onChangeEditText = event => {
+//     this.setState({ editText: event.target.value });
+//   };
 
-  render() {
-    const { authUser, pantry_item, onRemovePantryItem } = this.props;
-    const { editMode, editText } = this.state;
+//   render() {
+//     const { authUser, pantry_item, onRemovePantryItem } = this.props;
+//     const { editMode, editText } = this.state;
 
-    return (
-      <li>
-        {authUser.uid === pantry_item.userId && (
-          <span>
-            {editMode ? (
-              <input
-                type="text"
-                value={editText}
-                onChange={this.onChangeEditText}
-              />
-            ) : (
-                <span>
-                  <strong>
-                    {pantry_item.user || pantry_item.user}
-                  </strong>{' '}
-                  {pantry_item.text} {pantry_item.editedAt && <span>(Edited)</span>}
-                </span>
-              )}
+//     return (
+//       <li>
+//         {authUser.uid === pantry_item.userId && (
+//           <span>
+//             {editMode ? (
+//               <input
+//                 type="text"
+//                 value={editText}
+//                 onChange={this.onChangeEditText}
+//               />
+//             ) : (
+//                 <span>
+//                   <strong>
+//                     {pantry_item.user || pantry_item.user}
+//                   </strong>{' '}
+//                   {pantry_item.text} {pantry_item.editedAt && <span>(Edited)</span>}
+//                 </span>
+//               )}
 
-            {editMode ? (
-              <span>
-                <button onClick={this.onSaveEditText}>Save</button>
-                <button onClick={this.onToggleEditMode}>Reset</button>
-              </span>
-            ) : (
-                <button onClick={this.onToggleEditMode}>Edit</button>
-              )}
+//             {editMode ? (
+//               <span>
+//                 <button onClick={this.onSaveEditText}>Save</button>
+//                 <button onClick={this.onToggleEditMode}>Reset</button>
+//               </span>
+//             ) : (
+//                 <button onClick={this.onToggleEditMode}>Edit</button>
+//               )}
 
-            {!editMode && (
-              <button
-                type="button"
-                onClick={() => onRemovePantryItem(pantry_item.uid)}
-              >
-                Delete
-            </button>
-            )}
-          </span>
-        )}
-      </li>
-    );
-  }
+//             {!editMode && (
+//               <button
+//                 type="button"
+//                 onClick={() => onRemovePantryItem(pantry_item.uid)}
+//               >
+//                 Delete
+//             </button>
+//             )}
+//           </span>
+//         )}
+//       </li>
+//     );
+//   }
 
 
-}
+// }
 
 const PantryItems = withFirebase(PantryItemsBase);
 
