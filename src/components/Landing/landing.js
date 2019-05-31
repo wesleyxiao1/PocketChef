fb5932a2a6706278ed406c1c89e0a275897aa6ca
adhaile <nati@Natis-PC.localdomain>
2019-05-31 15:49:53 -0700

    forgot to swtich branch

--- src/components/Landing/landing.js
+++ src/components/Landing/landing.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Grid, Cell} from 'react-mdl'
+
 class Landing extends React.Component {
   render() {
-    return <h1>Landing</h1>
+    return (
+      <div style={{width: '80%', margin: 'auto'}}>
+        <div className="header">
+          <h1>Landing</h1>
+        </div>
+        <Grid className="demo-grid-1">
+            <Cell col={12}>4</Cell>
+            <Cell col={12}>4</Cell>
+            <Cell col={12}>4</Cell>
+        </Grid>
+        </div>
+
+
+    );
   }
 }
 export default Landing
