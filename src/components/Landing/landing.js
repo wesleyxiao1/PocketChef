66af59e265149ce6a2d5eda942d6b4482d547077
Kyle Johnson <johnsonkyle03@gmail.com>
2019-06-01 13:03:54 -0700

    also deleted mdl

--- src/components/Landing/landing.js
+++ src/components/Landing/landing.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Grid, Cell} from 'react-mdl'
+// import { Grid, Cell} from 'react-mdl'
 import './landing.css'
 
 class Landing extends React.Component {
