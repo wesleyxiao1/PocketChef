45a92db4fbde8134b0b18b1129c67e70dde67e4f
adhaile <adhaile@ucsd.edu>
2019-05-31 23:33:18 -0700

    dont judge this shit

--- src/components/Landing/landing.js
+++ src/components/Landing/landing.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react'
 import { Grid, Cell} from 'react-mdl'
+import './landing.css'
 
 class Landing extends React.Component {
   render() {
     return (
-      <div style={{width: '80%', margin: 'auto'}}>
-        <div className="header">
-          <h1>Landing</h1>
+      <div className="Landing-Style" style={{width: '100%', margin: 'auto'}}>
+        <div className="Header-PocketChef">
+          <h1 className="testing">Pocket Chef</h1>
+          <img src={require('../../images/poketche.JPG')} alt="pocket-Chef-Image" className="pocket-Chef-Image"/>
         </div>
-        <Grid className="demo-grid-1">
-            <Cell col={12}>4</Cell>
-            <Cell col={12}>4</Cell>
-            <Cell col={12}>4</Cell>
-        </Grid>
+
+
+        <div className="HowtoPocketChef">
+          <h2>How to Pocket Chef</h2>
+          <p>Three simples Steps</p>
         </div>
+
+        <div className="howToPocketChef">
+          <div className="Add" >
+            <h2>Add ingredients</h2>
+            <img src={require('../../images/icons8-add-64.png')}/>
+          </div>
+          
+          <div className="Search" > 
+            <h2>Search Recipes</h2>
+            <img src={require('../../images/search.png')}/>
+          </div>
+
+          <div className="Cook" >
+            <h2>Cook</h2>
+            <img src={require('../../images/cook.png')}/>
+          </div>
+
+        </div>
+        
+      </div>
 
 
     );
